139832d9681399d7d30589035fce5084c322ec84
Claudiu Teodor Filip <claudiucelfilip@gmail.com>
2019-07-17 20:09:28 +0300

    quickfix when running livrary on server side

--- src/main.js
+++ src/main.js
@@ -13,7 +13,10 @@ const TAG_STAKE = 3;
 const TAG_BATCH = 4;
 
 const JSBI = require('jsbi');
+
+const window = window || {};
 const BigInt = window.useNativeBigIntsIfAvailable ? BigInt: JSBI.BigInt;
+
 
 /**
  * Converts a string to a Buffer.
